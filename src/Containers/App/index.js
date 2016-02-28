cd17cb4254fd56b58e6f215738490cb1acb7b4b4
Arthur Flachs <arthur.flachs@gmail.com>
2016-02-28 20:49:16 +0100

    Uses Roboto Font for App Layout

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
+import styles from './styles.css'
 import Header from '../../Components/Header'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.App}>
         <Header />
       </div>
     )
