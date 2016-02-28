dfcd3564f1280cb9df7845e2837270528e95b64e
Arthur Flachs <arthur.flachs@gmail.com>
2016-02-28 20:48:56 +0100

    Moves Container App to its own folder

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import Header from '../Components/Header'
+import Header from '../../Components/Header'
 
 export default class App extends Component {
   render() {
